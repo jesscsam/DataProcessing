9bfa193add741fa30886662a700d395085978e7d
jesscsam <github@jessicasam.nl>
2019-05-21 16:52:34 +0200

    woop

--- Homework/Week_6/linkedviews.js
+++ Homework/Week_6/linkedviews.js
@@ -1,2 +1,16 @@
 
-  var map = new Datamap({element: document.getElementById('container')});
+var mapdata = "data.json"
+
+  var map = new Datamap({
+    element: document.getElementById('container'),
+
+    popupTemplate: function(geography, mapdata) {
+      return '<div class="hoverinfo">' + geography.properties.name + ' \
+        Total athletes:' +  data.Total + 'Female' + data.F + ' '
+    },
+    highlightBorderWidth: 3
+  },
+
+
+
+  });
