49d13385b37cb8649d73b6cd0cc0f5f4f781ddf7
jesscsam <github@jessicasam.nl>
2019-05-21 17:41:41 +0200

    update

--- Homework/Week_6/linkedviews.js
+++ Homework/Week_6/linkedviews.js
@@ -1,16 +1,27 @@
 
-var mapdata = "data.json"
+var mapdata = d3.json("data.json");
 
+d3v5.json("data.json").then(function(dataset){
+
+
+  console.log(dataset);
   var map = new Datamap({
     element: document.getElementById('container'),
+    data: dataset,
 
-    popupTemplate: function(geography, mapdata) {
-      return '<div class="hoverinfo">' + geography.properties.name + ' \
-        Total athletes:' +  data.Total + 'Female' + data.F + ' '
-    },
-    highlightBorderWidth: 3
-  },
+    geographyConfig: {
+
+
+      popupTemplate: function(geography, data) {
+
+        return '<div class="hoverinfo">' + geography.properties.name + ' \
+          <br> Total athletes: ' +  data.Total + ' <br> Female athletes: ' + data.F + ' \
+          <br> Male athletes: ' + data.M + ' '
+      },
+      highlightBorderWidth: 3
+  }
 
 
 
   });
+})
